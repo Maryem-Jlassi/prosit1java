66c78db62fbfc1158924faafce9d226d99e176a5
Maryem <Maryem.Jlassi@esprit.tn>
2025-01-28 16:21:29 +0100

    hello

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,34 +1,12 @@
 import java.util.Scanner;
 
 public class ZooManagement {
-    int nbrCage ;
-    String zooName ;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ZooManagement zoo = new ZooManagement();
-        System.out.print(" Veuillez entrer le nom du zoo : ");
-        zoo.zooName = scanner.nextLine();
+        Animal lion=new Animal("fhbh","lion",5,false);
+        Zoo myZoo=new Zoo("belvedere","tunis",10);
+        myZoo.addAnimal(lion,0);
+        myZoo.display();
 
-        while (true) {
-            System.out.print(" Veuillez entrer le nombre de cages (entier positif) : ");
-            if (scanner.hasNextInt()) {
-                zoo.nbrCage = scanner.nextInt();
-                if (zoo.nbrCage > 0) {
-                    break;
-                } else {
-                    System.out.println(" Erreur : Le nombre de cages doit être supérieur à 0.");
-                }
-            } else {
-                System.out.println(" Erreur : Veuillez entrer un entier valide.");
-                scanner.next();
-            }
-        }
-
-        System.out.println(zoo.zooName + " comporte " + zoo.nbrCage + " cages.");
-        scanner.close();
     }
 }
-
-
-
